039ebd2570cdbb27153c4a561d918e1486ebbb72
Paulo Nery <Paulo Nery@desktop-paulo>
2023-04-30 20:50:11 -0300

    Commit #2

--- MiniTesteOO/src/Empresa.java
+++ MiniTesteOO/src/Empresa.java
@@ -1,26 +1,27 @@
 
-public class Empresa {
- private int CEP;
- private String Endereco;
- private String CNPJ;
- 
-public int getCEP() {
-	return CEP;
-}
-public void setCEP(int cEP) {
-	CEP = cEP;
-}
-public String getEndereco() {
-	return Endereco;
-}
-public void setEndereco(String endereco) {
-	Endereco = endereco;
-}
-public String getCNPJ() {
-	return CNPJ;
-}
-public void setCNPJ(String string) {
-	CNPJ = string;
-}	
- 
-}
+public abstract class Empresa {
+		 private static int CEP;
+		 private  static String Endereco;
+		 private static String CNPJ;
+		 
+		public int getCEP() {
+			return CEP;
+		}
+		public static void setCEP(int cEP) {
+			CEP = cEP;
+		}
+		public String getEndereco() {
+			return Endereco;
+		}
+		public static void setEndereco(String endereco) {
+			Endereco = endereco;
+		}
+		public String getCNPJ() {
+			return CNPJ;
+		}
+		public static void setCNPJ(String string) {
+			CNPJ = string;
+		}	
+		 
+		}
+
--- MiniTesteOO/src/Microsoft.java
+++ MiniTesteOO/src/Microsoft.java
@@ -1,10 +1,8 @@
 
-public class Empresa3 extends Empresa {
+public class Microsoft extends Empresa {
 	public static void main(String[] args) {
-		Empresa Microsoft = new Empresa();
 		Microsoft.setCEP(1990194);
 		Microsoft.setEndereco("Lisboa");
 		 Microsoft.setCNPJ("04.712.500/0001-07"); 
 	}
-
 }
